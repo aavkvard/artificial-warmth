f652d3d16ba098ad6f714fad4b92f0d2a5b9e338
Peter Heiner <p.heiner@gmail.com>
2017-11-19 04:19:08 +0000

    add fireplace code

---
+++ fireplace.py
@@ -0,0 +1,63 @@
+#!/usr/bin/sudo python3
+
+import time
+import random
+
+try:
+    import unicornhathd as unicorn
+    print("unicorn hat hd detected")
+except ImportError:
+    from unicorn_hat_sim import unicornhathd as unicorn
+
+unicorn.set_layout(unicorn.AUTO)
+unicorn.rotation(180)
+unicorn.brightness(.8)
+#u_width,u_height=unicorn.get_shape()
+u_width,u_height=(8,8)
+
+palette = [
+	(0x00,0x00,0x00),
+	(0x00,0x00,0x00),
+	(0x00,0x00,0x00),
+	(0x45,0x00,0x00),
+	(0x5d,0x1a,0x00),
+	(0x83,0x25,0x00),
+	(0xb3,0x3e,0x00),
+	(0xed,0x7d,0x00),
+	(0xfc,0xad,0x00),
+	(0xff,0xc6,0x36),
+]
+
+board = [[0 for i in range(u_height)] for j in range(u_width)]
+pattern = [[-1,-1],[0,-1],[1,-1],[0,0]]
+
+def getavg(x,y):
+    colorsum = 0
+    for i in range(len(pattern)):
+        (tx, ty) = (x+pattern[i][0], y+pattern[i][1])
+        if tx>(len(board)-1):
+            tx = tx-(len(board)-1)
+        elif tx < 0:
+            tx = (len(board)-1)+tx
+        colorsum += board[tx][ty]
+    coloravg = colorsum//(len(pattern))
+    return coloravg
+
+
+while True:
+
+    board[random.randrange(len(board))][0] = random.randint(3,5)
+    board[random.randint(len(board)//2-len(board)//3, len(board)//2+len(board)//3)][0] = random.randint(7,8)
+
+    for y in range(1,u_height):
+        for x in range(u_width):
+            board[x][y] = getavg(x,y)
+
+    for y in range(u_height):
+        for x in range(u_width):
+            (r, g, b) = palette[board[x][y]]
+            unicorn.set_pixel(x, y, r, g, b)
+
+    unicorn.show()
+    time.sleep(0.02)
+
